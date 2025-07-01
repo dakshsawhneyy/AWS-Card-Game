4a5c7feacebec64d38c16dd2b1f32fa650327db6
dakshsawhneyy <darkstardaksh@gmail.com>
2025-07-01 16:31:09 +0000

    Created DynamoDB tables

---
+++ dynamo_db.py
@@ -0,0 +1,45 @@
+import boto3
+
+dynamodb = boto3.client('dynamodb')
+
+dynamodb.create_table(
+    TableName='GameSession',   
+    KeySchema=[
+        { 'AttributeName': 'GameID', 'KeyType': 'HASH' },  # Unique HASH key
+    ],
+    AttributeDefinitions=[
+        { 'AttributeName': 'GameID', 'AttributeType': 'S' },  # String type
+    ],  
+    ProvisionedThroughput={     # Capacity to provision  # Cost Optimization
+        'ReadCapacityUnits': 5,     # 5 read/write per second
+        'WriteCapacityUnits': 5
+    }
+)
+
+dynamodb.create_table(
+    TableName='Players'
+    KeySchema=[
+        { 'AttributeName': 'PlayerID', 'KeyType': 'HASH' },  # Unique HASH key
+    ],
+    AttributeDefinitions=[
+        { 'AttributeName': 'PlayerID', 'AttributeType': 'S' },  # String type
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,     # 5 read/write per second
+        'WriteCapacityUnits': 5
+    }
+)
+
+dynamodb.create_table(
+    TableName='Cards'
+    KeySchema=[
+        { 'AttributeName': 'CardID', 'KeyType': 'HASH' },  # Unique HASH key
+    ],
+    AttributeDefinitions=[
+        { 'AttributeName': 'CardID', 'AttributeType': 'S' },  # String type
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,     # 5 read/write per second
+        'WriteCapacityUnits': 5
+    }
+)
