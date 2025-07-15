c5b36b16ff2d56442fba635a2f8ee6b98330ebee
Daksh Sawhney <darkstardaksh@gmail.com>
2025-07-15 17:47:37 +0530

    Added new special card

--- card-battle-arena/src/components/Card.jsx
+++ card-battle-arena/src/components/Card.jsx
@@ -3,10 +3,11 @@ import attack from "../assets/attack.png"
 import heal from "../assets/heal.png"
 import defense from "../assets/defense.png"
 import special from "../assets/special.png"
+import special2 from "../assets/special2.png"
 
 const Card = ({card, onThrow, disabled}) => {
   return (
-    <div className={`w-32 h-48 shadow-lg cursor-pointer transition transform hover:scale-105 ${disabled ? "opacity-50 cursor-not-allowed" : ""}`} onClick={!disabled ? () => onThrow(card.CardID,card.Type) : undefined} >
+    <div className={`w-32 h-48 shadow-lg cursor-pointer transition drop-shadow-3xl transform hover:scale-105 ${disabled ? "opacity-50 cursor-not-allowed" : ""}`} onClick={!disabled ? () => onThrow(card.CardID,card.Type) : undefined} >
       {/* Differentiating Cards as jsx doesnt have if statements */}
       {card.Type == 'attack' && (
         <img src={attack} alt="Attack Card" className="w-full h-full rounded object-cover" />
@@ -18,7 +19,7 @@ const Card = ({card, onThrow, disabled}) => {
         <img src={heal} alt="Heal Card" className="w-full h-full rounded object-cover" />
       )}
       {card.Type == 'special' && (
-        <img src={special} alt="Attack Card" className="w-full h-full rounded object-cover" />
+        <img src={special2} alt="Attack Card" className="w-full h-full rounded object-cover" />
       )}
       
     </div>
